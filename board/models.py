3ad22b392bb1a32c3c4b49cff168a93b2b0bc986
hora2017 <ho_ra@naver.com>
2017-12-08 14:55:29 +0900

    선생님 파일 클론/게시판연동 완료/수정 이슈 해결

--- board/models.py
+++ board/models.py
@@ -1,29 +1,31 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    modified_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
 
-# class Comment(models.Model):
-#     post = models.ForeignKey('board.Post', related_name='comments')
-#     author = models.CharField(max_length=200)
-#     text = models.TextField()
-#     created_date = models.DateTimeField(default=timezone.now)
-#     approved_comment = models.BooleanField(default=False)
+class Comment(models.Model):
+    post = models.ForeignKey('board.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
 
-#     def approve(self):
-#         self.approved_comment = True;
-#         self.save()
-    
-#     def __str__(self):
-#         return self.text
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
